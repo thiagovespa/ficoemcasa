fc28320d730109a6e798482b470b55d2b6e773fa
Diego Hernandes Ferraz <diegohferraz@hotmail.com>
2020-03-26 20:28:38 -0300

    project cleanup

---
+++ ficoemcasa-app/src/pages/Categories/index.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+
+const Categoria = () => {
+  return ( 
+    <Fragment>
+      <h1>Categoria</h1>
+      <Link to="/">Voltar</Link>
+    </Fragment>
+   );
+}
+ 
+export default Categoria;
